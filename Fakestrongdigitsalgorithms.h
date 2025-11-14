9c6e9048b521fefdee54c0c46f0155133dfad44b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-14 05:19:39 +0100

    Create Fakestrongdigitsalgorithms.h

---
+++ Fakestrongdigitsalgorithms.h
@@ -0,0 +1,27 @@
+//we can see truths with latent vars from oarc to darpa models
+//if we collect ui/ux models and .. models.
+//notice this is a trouble to say that.
+//so wed better start with topologies anomalies
+
+class checkanomaliesoncontractsandoptimizeonelineifplotplotalgorithm
+{
+
+}
+
+//Now thé second part would bé to créate a plot plot log fonction mapper from pro p groups to abelian groups because this IS not a common behaviour. So thé resulting ai would bé more stable to unknown input messages on bios ai. But WE need 10^3 latent variables to even créate a third output model. Thats it ! Because de deal with 10^15 MeV systèms !
+Class systemgetlessoutputlatentvqriables_secure
+{
+
+}
+
+
+//According to chatgpt E need .... Uiux datas to créate signals that are stable in a high energy systèm. Notice WE dont speek about ...
+
+
+//Suite safran micro rubidium atomic clocks
+https://en.wikipedia.org/wiki/Hyperfine_structure#Use_in_defining_the_SI_second_and_meter
+
+
+//Now lets deal with decoding quantité. Discord physics forum..
+
+Class decodingquantityformaxsloonucleartheoremandlemmasfirstwithmasstransferalgorithmnotprovidedprocedd
